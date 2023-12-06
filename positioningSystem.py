53c15a341956a0ecb0ce33f87a2915c0bbb687ef
elmirsoftic <elmosofti199@hotmail.com>
2023-12-06 15:47:42 +0100

    new Positioning System

--- positioningSystem.py
+++ positioningSystem.py
@@ -2,4 +2,6 @@ from speedometer import speedometer
 from gyroskop import gyro
 
 class positioningSystem:
-    def __init__(self, gyroAddress,):
+    def __init__(self, gyroAddress,hallPinForward,hallPinBackward):
+        self.gyroskop = gyro(gyroAddress)
+        self.speedometer = speedometer(hallPinForward,hallPinBackward)

11db58676b4a630e35b6d894b6a1d7654341bd23
elmirsoftic <elmosofti199@hotmail.com>
2023-12-06 19:45:49 +0100

    changing setDistance function for speedometer

--- speedometer.py
+++ speedometer.py
@@ -95,9 +95,9 @@ try:
     while True:
         speedometerOne.setCount()
         time.sleep(1)
-        #print("Pin Forward:", GPIO.input(17))
-        #print("Pin Back:", GPIO.input(27))
-        #print(speedometerOne.getCount())
+        print("Pin Forward:", GPIO.input(17))
+        print("Pin Back:", GPIO.input(27))
+        print(speedometerOne.getCount())
         speedometerOne.checkDirectionTire()
         currenDistance = (speedometerOne.getCount() * ((speedometerOne.getWheel() * pi) / 4))
         speedometerOne.setDistance(currenDistance)

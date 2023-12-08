c1a8dea48c26bc7aa834baa22180569c92ff7acd
elmirsoftic <elmosofti199@hotmail.com>
2023-12-08 19:55:13 +0100

    set Default wait for gyroTest

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from Navigation.Navigation import navigation
+import RPi.GPIO as GPIO
 
 if __name__ == "__main__":
     navigation = navigation()
+    try:
+        while True:
+            print("Geschwindigkeit des Fahrzeugs: ",navigation.positioningSystem.speedometer.getSpeed(),"km/h")
+            print("Gefahrene Distanz: ", navigation.positioningSystem.speedometer.getDistance(),"m")
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()

85c833fae0d06d1c65088444bce8298dd7fa5e07
elmirsoftic <elmosofti199@hotmail.com>
2023-12-09 21:22:31 +0100

    added sleep in GyroTest2.py

--- PositioningSystem/gyro3.py
+++ PositioningSystem/gyro3.py
@@ -1,70 +1,13 @@
-import smbus
 import time
+import board
+import adafruit_mpu6050
 
-ACCEL_XOUT_H = 0x3B
-ACCEL_XOUT_L = 0x3C
-ACCEL_YOUT_H = 0x3D
-ACCEL_YOUT_L = 0x3E
-ACCEL_ZOUT_H = 0x3F
-ACCEL_ZOUT_L = 0x40
+i2c = board.I2C()
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
-
-bus = smbus.SMBus(1)
-
-
-def read_word(reg):
-    high = bus.read_byte_data(0x68, reg)
-    low = bus.read_byte_data(0x68, reg + 1)
-    value = (high << 8) + low
-    return value
-
-
-def read_acceleration_data():
-
-    accel_x = read_word(ACCEL_XOUT_H)
-    accel_y = read_word(ACCEL_YOUT_H)
-    accel_z = read_word(ACCEL_ZOUT_H)
-
-
-    if accel_x > 32767:
-        accel_x -= 65536
-    if accel_y > 32767:
-        accel_y -= 65536
-    if accel_z > 32767:
-        accel_z -= 65536
-
-    return accel_x, accel_y, accel_z
-
-
-try:
-    initial_time = time.time()
-    initial_velocity_x = 0
-
-    while True:
-
-        accel_data = read_acceleration_data()
-        accel_x, _, _ = accel_data
-
-
-        acceleration_x = 9.81 * accel_x
-
-
-        current_time = time.time()
-        time_elapsed = current_time - initial_time
-
-
-        velocity_x = initial_velocity_x + acceleration_x * time_elapsed
-
-
-        print("Geschwindigkeit in Metern pro Sekunde (X-Achse):", velocity_x)
-
-
-        initial_velocity_x = velocity_x
-        initial_time = current_time
-
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    pass
-
-finally:
-    bus.close()
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)

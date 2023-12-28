9a7bdcf6efde862341a2ea6ff008b5ae988e262c
elmirsoftic <elmosofti199@hotmail.com>
2023-12-28 14:30:35 +0100

    added new calc function for getting navigation instructions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
-sys.path.append("Navigation")
-import navigation
-#from Navigation.Navigation import navigation
+sys.path.append("/root")
+from Navigation.Navigation import navigation
 
 if __name__ == "__main__":
     navigation = navigation()

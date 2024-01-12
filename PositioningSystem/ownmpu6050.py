f67b91fadd7ab669d4c8b35177d88ef5c5ad2696
elmirsoftic <elmosofti199@hotmail.com>
2024-01-12 17:25:38 +0100

    comment all functions and code

--- PositioningSystem/ownmpu6050.py
+++ PositioningSystem/ownmpu6050.py
@@ -6,7 +6,7 @@ import mpu6050
 
 # self implemented mpu6050 class
 class OwnMpu6050:
-    def __init__(self, gyro_range=2000, filter_range=256):
+    def __init__(self, gyro_range=2000, filter_range=5):
         self.mpu6050_address = mpu6050Address  # mpu6050 address
         self.bus = smbus.SMBus(smbusPort)  # creating smbus port
         self.bus.write_byte_data(self.mpu6050_address, pwrmgmt, 0x00)  # wake up MPU-6050
